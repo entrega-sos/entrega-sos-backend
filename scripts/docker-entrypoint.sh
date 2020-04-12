7829aa102d003d9e4eca1191dd581d297cd01626
Eduardo Daltro <edaltro23@gmail.com>
2020-04-12 19:47:39 -0300

    docker-entrypoint atualizado

--- scripts/docker-entrypoint.sh
+++ scripts/docker-entrypoint.sh
@@ -12,6 +12,4 @@ while true; do
     sleep 5
 done
 
-# exec gunicorn --bind 0.0.0.0:5000 --reload --access-logfile - --error-logfile - wsgi:app
-
 exec gunicorn --workers=4 --bind 0.0.0.0:5000 wsgi:app --reload --access-logfile - --error-logfile - --timeout 5 --keep-alive 5
